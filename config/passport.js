14707308b15f1909235bb236d9eaff3c8061fce9
Carlos Feliciano <c.feliciano2009@gmail.com>
2014-02-27 23:20:17 -0400

    Replaced coffee with js to keep it more js-like also added twitter support but it recreates the user after each login

---
+++ config/passport.js
@@ -0,0 +1,77 @@
+var passport        = require("passport")
+  , LocalStrategy   = require("passport-local").Strategy
+  , bcrypt          = require("bcrypt")
+  , TwitterStrategy = require("passport-twitter").Strategy;
+
+passport.serializeUser(function(user, done) {
+  console.log('serializer', user);
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+/*
+ Passport Local Strategy
+*/
+
+passport.use(new LocalStrategy(function(email, password, done) {
+  User.findByEmail(email).done(function(err, user) {
+    if (err) {
+      return done(null, err);
+    }
+    if (!user || user.length < 1) {
+      return done(null, false, {
+        message: "Incorrect User"
+      });
+    }
+    bcrypt.compare(password, user[0].password, function(err, res) {
+      if (!res) {
+        return done(null, false, {
+          message: "Invalid Password"
+        });
+      }
+      return done(null, user);
+    });
+  });
+}));
+
+/*
+ Passport Twitter Strategy
+*/
+
+passport.use(new TwitterStrategy({
+  consumerKey: process.env.TWITTER_CONSUMER_KEY,
+  consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+  callbackURL: "http://127.0.0.1:1337/auth/twitter/callback"
+  },
+  function(token, tokenSecret, profile, done) {
+    User.findOrCreate({
+      username: profile.displayName
+    },
+    function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      done(null, user);
+    });
+  }
+));
+
+/*
+ Initialize Passport
+*/
+
+module.exports = {
+  express: {
+    customMiddleware: function(app) {
+      console.log("express midleware for passport");
+      app.use(passport.initialize());
+      app.use(passport.session());
+    }
+  }
+};
